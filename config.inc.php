4f21f473bc8fe1aa2d680d9a02fbeef1dedf10df
xuxalegacy <xuxaparkespanha@gmail.com>
2026-07-22 00:44:05 -0400

    Create config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,11 @@
+<?php
+// Coppermine configuration file
+// Database configuration
+$CONFIG['dbtype'] =      'mysqli';		// Your database type
+$CONFIG['dbserver'] =    'db';			// Your database server
+$CONFIG['dbuser'] =      'coppermine';			// Your database username
+$CONFIG['dbpass'] =      'coppermine';			// Your database password
+$CONFIG['dbname'] =      'coppermine';			// Your database name
+
+// DATABASE TABLE NAMES PREFIX
+$CONFIG['TABLE_PREFIX'] =         'CPG_';
